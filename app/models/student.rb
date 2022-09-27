b358710de390b216d36518c989b187fae58ea5c4
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-27 05:55:13 +0300

    COMPLETED: Optional - Route Params Lab

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,11 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  # handling query params
+  # =====================
+  def self.search_by_name(name)
+    #SQL => (%) denotes zero or more character matching
+    Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+  end
+
 end
