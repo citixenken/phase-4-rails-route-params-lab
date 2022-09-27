cbf766a5e988e6b49ca2e4a29a6e9b2a5a79550f
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-27 05:52:28 +0300

    commit tree 8c5b7d2c6d19101af3139c3913eae9da76915160

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,29 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # query params
+    #=============
+    if params[:name]
+      students = Student.search_by_name(params[:name])
+    else
+      students = Student.all
+    end
     render json: students
   end
 
+  def grades
+    student = Student.order(grade: :desc)
+    render json: student
+  end
+
+  def highest_grade
+    student = Student.order(grade: :desc).first
+    render json: student
+  end
+
+  # dynamic routing
+  def student 
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+
+  # dynamic routing
+  get '/students/:id', to: 'students#student'
+
 end
